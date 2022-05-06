838ec0be8b3046375bdfa81d3a3dfdd6d9723c20
Matsushita Takeshi <matsushitatakeshi@team-lab.com>
2022-05-07 00:58:22 +0900

    add client config

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    outputStandalone: true,
+  },
   reactStrictMode: true,
-}
+  trailingSlash: true,
+  distDir: 'dist',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
